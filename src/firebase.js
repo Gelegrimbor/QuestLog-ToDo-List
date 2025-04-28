35d3ff453fc808fc1b9b92d3aaecd21f862ca6fb
Joshua <joshuas1177@gmail.com>
2025-04-28 21:56:08 +0800

    changes to firebase updated

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCAlfl78R7jRbudOqMRbgYzfpLo1iJJ6Hc',
-  authDomain: 'questlog-efae2.firebaseapp.com',
-  projectId: 'questlog-efae2',
-  storageBucket: 'questlog-efae2.appspot.com',
-  messagingSenderId: '717808827893',
-  appId: '1:717808827893:web:b151616f604636ab5674bc',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
